a3754b5d76add9683c32e2f2d10bdead65a4d4ae
forlabi <lukman_afolabi@outlook.com>
2023-10-30 22:32:18 +0100

    MVP: Calculates distance between two cordinates

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
-// function haversineDistance(cord1, cord2) {
-//     const radius = 6371; // Radius of the Earth in kilometers;
-//     const [lat1, lon1] = cord1.split(/[, ]/);
-//     const [lat2, lon2] = cord2.split(/[, ]/);
+const calButton = document.getElementById('btn');
+
+calButton.addEventListener("click", function(event){
+    event.preventDefault();
+    const startCord = document.getElementById('start').value;
+    const stopCord = document.getElementById('stop').value;
+    const result = document.getElementById('result');
+
+   result.textContent = haversineDistance(startCord, stopCord);
+   
+
+});
 
 
-//     //Convert latitude and longitude from degrees to radians
-//     const radLat1 = (Math.PI * lat1) / 180;
-//     const radLon1 = (Math.PI * lon1) / 180;
-//     const radLat2 = (Math.PI * lat2) / 180;
-//     const radLon2 = (Math.PI * lon2) / 180;
 
-//     // Haversine formula
-//     const dLat = radLat2 - radLat1;
-//     const dLon = radLon2 - radLon1;
-//     const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-//               Math.cos(radLat1) * Math.cos(radLat2) *
-//               Math.sin(dLon / 2) * Math.sin(dLon / 2);
-//     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+function haversineDistance(cord1, cord2) {
+    const radius = 6371; // Radius of the Earth in kilometers;
+    const [lat1, lon1] = cord1.split(/[, ]/);
+    const [lat2, lon2] = cord2.split(/[, ]/);
 
-//     // Calculate the distance
-//     const distance = radius * c;
 
-//     return distance;
-// }
+    //Convert latitude and longitude from degrees to radians
+    const radLat1 = (Math.PI * lat1) / 180;
+    const radLon1 = (Math.PI * lon1) / 180;
+    const radLat2 = (Math.PI * lat2) / 180;
+    const radLon2 = (Math.PI * lon2) / 180;
+
+    // Haversine formula
+    const dLat = radLat2 - radLat1;
+    const dLon = radLon2 - radLon1;
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+              Math.cos(radLat1) * Math.cos(radLat2) *
+              Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    // Calculate the distance
+    const distance = radius * c;
+
+    return distance
+}
 
 
 // function calculateDistances(singleCoordinate, coordinatesArray) {
@@ -37,11 +52,7 @@
 // }
 
 
-// // Example usage
-// const cord1 = "4.802933 7.0382";  // Latitude of point 1
-// const cord2 = "4.81934 7.04007";  // Latitude of point 2
 
 
 
-// const distance = haversineDistance(cord1, cord2);
-// console.log('Distance: ${distance.toFixed(2)} km');
+
